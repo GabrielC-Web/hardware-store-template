55da1308db113c6269b1969ed0d715a3a5aedae2
GabrielC-Web <gabrieljose.cz21@gmail.com>
2024-04-08 21:51:36 -0400

    Añadida descripción de producto

--- src/app/modules/products/components/products-detail/products-detail.component.ts
+++ src/app/modules/products/components/products-detail/products-detail.component.ts
@@ -21,7 +21,19 @@ export class ProductsDetailComponent {
     sku: 'KROGGM93746917',
     partNumber: '93746917',
     maximunPurchase: 1,
-    quantity: 1
+    quantity: 1,
+    description: 'Repuesto Original General Motors',
+    aditionalDetails: {
+      size: 'Dimensiones 9x30x9cm3',
+      weight: '0,9 kg.',
+      applicationModels: [
+        '2005-2014 | CHEVROLET | AVEO | 3 PUERTAS',
+        '2005-2014 | CHEVROLET | AVEO | 3 PUERTAS LT',
+        '2005-2014 | CHEVROLET | AVEO | 4 PUERTAS',
+        '2005-2014 | CHEVROLET | AVEO | 4 PUERTAS LT',
+        '2005-2014 | CHEVROLET | AVEO | 5 PUERTAS',
+      ]
+    }
   }
 
 }
